f88303ef4c9390af30e5d159448b21f287e8df01
jfcaballerop <jfcaballerop@gmail.com>
2022-01-28 19:27:52 +0100

    add openapi complete

---
+++ docs/customers/get-customers-activated.js
@@ -0,0 +1,20 @@
+module.exports = {
+	get: {
+		tags: ['Customer CRUD operations'],
+		description: "Get all activated customers",
+		operationId: 'getCustomersActivated',
+		parameters: [],
+		responses: {
+			'200': {
+				description: "All Customers activated were obtained",
+				content: {
+					'application/json': {
+						schema: {
+							$ref: '#/components/schemas/Customer'
+						}
+					}
+				}
+			}
+		}
+	}
+}
--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,7 @@
 
 const basicInfo = require('./basicInfo');
+const components = require('./components');
+const customers = require('./customers');
 const servers = require('./servers');
 const tags = require('./tags');
 
@@ -7,5 +9,7 @@ const tags = require('./tags');
 module.exports = {
 	...basicInfo,
 	...servers,
-	...tags
+	...tags,
+	...components,
+	...customers
 }
