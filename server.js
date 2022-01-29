8cdcab49749d584a2dd4cc5870726e83a55b972a
jfcaballerop <jfcaballerop@gmail.com>
2022-01-29 22:43:13 +0100

    add API test

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.get('/', (req, res) => {
 require('./src/routes/customer.routes')(app)
 
 // set port, listen for requests
-const PORT = process.env.NODE_DOCKER_PORT
+const PORT = (process.env.NODE_ENV === 'test') ? process.env.NODE_TEST_LOCAL_PORT : process.env.NODE_DOCKER_PORT
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`)
 })
@@ -61,3 +61,5 @@ process.on('unhandledRejection', err => {
   console.warn('OOOPS! Unhandled Promise rejection', err.message)
   console.log(err)
 })
+
+module.exports = app
