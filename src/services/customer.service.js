ca02da85dd8b5365e8709e61af2dd987863c8800
jfcaballerop <jfcaballerop@gmail.com>
2022-01-30 20:44:05 +0100

    Add integration Test for customer

--- src/services/customer.service.js
+++ src/services/customer.service.js
@@ -206,8 +206,9 @@ const update = async (req) => {
 // Delete a Customer with the specified id in the request
 const deleteOne = async (req) => {
   const id = req.params.id
+  console.log('ID DELETE::', id)
 
-  await Customer.findByIdAndDelete(id, req.body, { useFindAndModify: false })
+  await Customer.findByIdAndDelete(id)
     .then(data => {
       if (!data) {
         customerResponse = {
